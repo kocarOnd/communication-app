b6749f45b49c02ae5096d41777508d819a920a4e
Onk Ar <kocarond@gmail.com>
2026-07-21 12:42:35 +0200

    Added the maven structure for the back-end server

---
+++ backend-server/comm-app-backend/src/main/java/cz/cuni/mff/kocaro/programs/comm_app_backend/CommAppBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.kocaro.programs.comm_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend-server/comm-app-backend/src/test/java/cz/cuni/mff/kocaro/programs/comm_app_backend/CommAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.kocaro.programs.comm_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
