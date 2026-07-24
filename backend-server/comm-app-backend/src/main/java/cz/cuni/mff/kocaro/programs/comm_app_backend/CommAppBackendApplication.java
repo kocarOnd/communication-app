3b06cb334a2b4f03dccd023aa19a24154a2098e4
Onk Ar <kocarond@gmail.com>
2026-07-24 19:44:45 +0200

    Configured my server to accept requests from my own machine

--- backend-server/comm-app-backend/src/main/java/cz/cuni/mff/kocaro/programs/comm_app_backend/CommAppBackendApplication.java
+++ backend-server/comm-app-backend/src/main/java/cz/cuni/mff/kocaro/programs/comm_app_backend/CommAppBackendApplication.java
@@ -2,6 +2,9 @@ package cz.cuni.mff.kocaro.programs.comm_app_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CommAppBackendApplication {
@@ -9,5 +12,21 @@ public class CommAppBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommAppBackendApplication.class, args);
 	}
+
+	/**
+	 * Allows requests from my own machine
+	 * @return WebMvcConfigurer: Configurer with added CORS mapping
+	 */
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 }
