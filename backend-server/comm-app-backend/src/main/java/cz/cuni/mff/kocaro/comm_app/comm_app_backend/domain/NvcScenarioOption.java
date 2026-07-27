a982369dba132de167169fe6be55ef96f4287eff
Onk Ar <kocarond@gmail.com>
2026-07-27 13:32:49 +0200

    Renamed the classes to allow easier extensions in the future

---
+++ backend-server/comm-app-backend/src/main/java/cz/cuni/mff/kocaro/comm_app/comm_app_backend/domain/NvcScenarioOption.java
@@ -0,0 +1,45 @@
+package cz.cuni.mff.kocaro.comm_app.comm_app_backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "nvc_scenario_options")
+@Getter
+@Setter
+@NoArgsConstructor
+public class NvcScenarioOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NvcPhase phase;
+
+    @Column(nullable = false, length = 256)
+    private String text;
+
+    @Column(nullable = false)
+    private boolean isCorrect;
+
+    @Column(length = 1024)
+    private String feedback; 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nvc_scenario_id", nullable = false)
+    private NvcScenario scenario;
+}
