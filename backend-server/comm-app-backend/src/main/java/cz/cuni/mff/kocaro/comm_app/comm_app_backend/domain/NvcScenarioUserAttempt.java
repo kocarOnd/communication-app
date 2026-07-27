562015ceb69beadbcf411284cef56ec87570b410
Onk Ar <kocarond@gmail.com>
2026-07-27 13:34:19 +0200

    Added a database entity for logging user attempts

---
+++ backend-server/comm-app-backend/src/main/java/cz/cuni/mff/kocaro/comm_app/comm_app_backend/domain/NvcScenarioUserAttempt.java
@@ -0,0 +1,41 @@
+package cz.cuni.mff.kocaro.comm_app.comm_app_backend.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class NvcScenarioUserAttempt {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String deviceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "scenario_id", nullable = false)
+    private NvcScenario scenario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "selected_option_id", nullable = false)
+    private NvcScenarioOption selectedOption;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NvcPhase phase;
+
+    @Column(nullable = false)
+    private boolean wasCorrect;
+
+    @Column(nullable = false)
+    private LocalDateTime attemptedAt = LocalDateTime.now();
+}
